c9dcbb8cd028245ec2d02c4778c650caf302a37f
GigaTortilla <martinappel01@gmail.com>
2024-03-19 16:40:57 +0100

    added verfier views

--- src/main/java/org/gigatortilla/isbn/verifier/ISBNwindow.java
+++ src/main/java/org/gigatortilla/isbn/verifier/ISBNwindow.java
@@ -3,6 +3,7 @@ package org.gigatortilla.isbn.verifier;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -10,15 +11,57 @@ import java.nio.file.Paths;
 import javax.imageio.ImageIO;
 
 public class ISBNwindow {
-    ActionListener switchISBN10validate = new ActionListener() {
+    private Frame mainWindowFrame;
+    private int windowWidth = 400;
+    private int windowHeight = 300;
+
+    private ActionListener switchISBN10validate = new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-            // TODO: Switch to ISBN-10 input
+            mainWindowFrame.removeAll();
+
+            Button returnButton = new Button("Back");
+            Label hintLabel = new Label("Please input the ISBN-10 number to check:", Label.LEFT);
+            TextField inputNumberField = new TextField(10);
+
+            returnButton.setBounds(320, 250, 60, 30);
+            returnButton.setBackground(new Color(240, 240, 240));
+            hintLabel.setBounds(50, 80, 250, 25);
+            inputNumberField.setBounds(50, 110, 80, 25);
+
+            returnButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    createStartupScreen();
+                };
+            });
+
+            mainWindowFrame.add(returnButton);
+            mainWindowFrame.add(hintLabel);
+            mainWindowFrame.add(inputNumberField);
         }
     };
 
-    ActionListener switchISBN13validate = new ActionListener() {
+    private ActionListener switchISBN13validate = new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-            // TODO: Switch to ISBN-13 input
+            mainWindowFrame.removeAll();
+
+            Button returnButton = new Button("Back");
+            Label hintLabel = new Label("Please input the ISBN-13 number to check:", Label.LEFT);
+            TextField inputNumberField = new TextField(13);
+
+            returnButton.setBounds(320, 250, 60, 30);
+            returnButton.setBackground(new Color(240, 240, 240));
+            hintLabel.setBounds(50, 80, 250, 25);
+            inputNumberField.setBounds(50, 110, 104, 25);
+
+            returnButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    createStartupScreen();
+                };
+            });
+
+            mainWindowFrame.add(returnButton);
+            mainWindowFrame.add(hintLabel);
+            mainWindowFrame.add(inputNumberField);
         }
     };
 
@@ -32,41 +75,58 @@ public class ISBNwindow {
             e.printStackTrace();
         }
 
-        Frame f = new Frame("ISBN Verifier");
+        mainWindowFrame = new Frame("ISBN Verifier");
+        mainWindowFrame.setIconImage(bufferedIcon);
+        mainWindowFrame.setSize(windowWidth, windowHeight);
+        mainWindowFrame.setBackground(new Color(192, 192, 192));
+        mainWindowFrame.setLayout(null);
+
+        // Close window with the red closing button
+        mainWindowFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent wEvent) {
+                mainWindowFrame.dispose();
+            }
+        });
+    }
+
+    public void createStartupScreen() {
+        mainWindowFrame.removeAll();
+        
         Label welcomeMessage = new Label("Welcome to the ISBN Verifier!");
         TextField inField = new TextField();
         Button submitButton = new Button("Submit");
-
-        // Close window with the red closing button
-        f.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent wEvent) {
-                f.dispose();
-            }
-        });
+        Button buttonISBN10verify = new Button("ISBN-10");
+        Button buttonISBN13verify = new Button("ISBN-13");
         
         welcomeMessage.setBounds(100, 70, 200, 30);
         welcomeMessage.setAlignment(Label.CENTER);
         inField.setBounds(120, 100, 80, 30);
         submitButton.setBounds(200, 100, 80, 30);
+        submitButton.setBackground(new Color(240, 240, 240));
+        buttonISBN10verify.setBounds(120, 180, 70, 30);
+        buttonISBN10verify.setBackground(new Color(240, 240, 240));
+        buttonISBN13verify.setBounds(200, 180, 70, 30);
+        buttonISBN13verify.setBackground(new Color(240, 240, 240));
 
         submitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                inField.setText("Welcome");
+                inField.setText("Lorem ipsum");
             }
         });
+        buttonISBN10verify.addActionListener(switchISBN10validate);
+        buttonISBN13verify.addActionListener(switchISBN13validate);
 
-        f.add(welcomeMessage);
-        f.add(inField);
-        f.add(submitButton);
-
-        f.setSize(400, 300);
-        f.setIconImage(bufferedIcon);
-        f.setBackground(new Color(192, 192, 192));
-        f.setLayout(null);
-        f.setVisible(true);
+        mainWindowFrame.add(welcomeMessage);
+        mainWindowFrame.add(inField);
+        mainWindowFrame.add(submitButton);
+        mainWindowFrame.add(buttonISBN10verify);
+        mainWindowFrame.add(buttonISBN13verify);
+        
+        mainWindowFrame.setVisible(true);
     }
     
     public void close() {
         System.out.println("Window close method called.");
+        mainWindowFrame.dispose();
     }
 }
--- src/main/java/org/gigatortilla/isbn/verifier/Main.java
+++ src/main/java/org/gigatortilla/isbn/verifier/Main.java
@@ -1,10 +1,8 @@
 package org.gigatortilla.isbn.verifier;
 
-import java.nio.file.Paths;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Paths.get("./src/main/resources/coding.png").toAbsolutePath().toString());
         ISBNwindow window = new ISBNwindow();
+        window.createStartupScreen();
     }
 }
