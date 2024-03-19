6725422e1362a1f8eb9c3487e237123a1796b25e
GigaTortilla <martinappel01@gmail.com>
2024-03-19 11:28:04 +0100

    added event listener

--- src/main/java/org/gigatortilla/isbn/verifier/ISBNwindow.java
+++ src/main/java/org/gigatortilla/isbn/verifier/ISBNwindow.java
@@ -1,24 +1,34 @@
 package org.gigatortilla.isbn.verifier;
 
-import java.awt.*; 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
 
-public class ISBNwindow {
+public class ISBNwindow implements ActionListener{
+    TextField inField;
+
     ISBNwindow() {
         Frame f = new Frame("ISBN Verifier");
         Label welcomeMessage = new Label("Welcome to the ISBN Verifier!");
-        TextField inField = new TextField();
-        Button submiButton = new Button("Submit");
+        inField = new TextField();
+        Button submitButton = new Button("Submit");
 
-        welcomeMessage.setBounds(20, 80, 80, 30);
+        welcomeMessage.setBounds(20, 70, 160, 30);
         inField.setBounds(20, 100, 80, 30);
-        submiButton.setBounds(100, 100, 80, 30);
+        submitButton.setBounds(100, 100, 80, 30);
+
+        submitButton.addActionListener(this);
 
         f.add(welcomeMessage);
         f.add(inField);
-        f.add(submiButton);
+        f.add(submitButton);
 
         f.setSize(400, 300);
         f.setLayout(null);
         f.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        inField.setText("Welcome");
     }
 }
