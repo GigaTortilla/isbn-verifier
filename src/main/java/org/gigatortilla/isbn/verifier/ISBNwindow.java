955a96de962adad0ef170ebf4fe235c2c9605957
GigaTortilla <martinappel01@gmail.com>
2024-03-19 15:02:30 +0100

    added window icon

--- src/main/java/org/gigatortilla/isbn/verifier/ISBNwindow.java
+++ src/main/java/org/gigatortilla/isbn/verifier/ISBNwindow.java
@@ -1,21 +1,49 @@
 package org.gigatortilla.isbn.verifier;
 
 import java.awt.*;
-import java.awt.event.*; 
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
 
 public class ISBNwindow {
+    ActionListener switchISBN10validate = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            // TODO: Switch to ISBN-10 input
+        }
+    };
+
+    ActionListener switchISBN13validate = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            // TODO: Switch to ISBN-13 input
+        }
+    };
+
     ISBNwindow() {
+        File iconFile = new File(Paths.get(".\\src\\main\\resources\\coding.png").toAbsolutePath().toString());
+        iconFile.setReadOnly();
+        BufferedImage bufferedIcon = new BufferedImage(48, 48, BufferedImage.TYPE_4BYTE_ABGR);
+        try {
+            bufferedIcon = ImageIO.read(iconFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Frame f = new Frame("ISBN Verifier");
         Label welcomeMessage = new Label("Welcome to the ISBN Verifier!");
         TextField inField = new TextField();
         Button submitButton = new Button("Submit");
 
+        // Close window with the red closing button
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent wEvent) {
                 f.dispose();
             }
         });
-
+        
         welcomeMessage.setBounds(100, 70, 200, 30);
         welcomeMessage.setAlignment(Label.CENTER);
         inField.setBounds(120, 100, 80, 30);
@@ -32,7 +60,8 @@ public class ISBNwindow {
         f.add(submitButton);
 
         f.setSize(400, 300);
-        f.setIconImage(null);
+        f.setIconImage(bufferedIcon);
+        f.setBackground(new Color(192, 192, 192));
         f.setLayout(null);
         f.setVisible(true);
     }
--- src/main/java/org/gigatortilla/isbn/verifier/Main.java
+++ src/main/java/org/gigatortilla/isbn/verifier/Main.java
@@ -1,8 +1,10 @@
 package org.gigatortilla.isbn.verifier;
 
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Paths.get("./src/main/resources/coding.png").toAbsolutePath().toString());
         ISBNwindow window = new ISBNwindow();
     }
 }
