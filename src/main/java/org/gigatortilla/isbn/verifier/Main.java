59c1e17bdd3a1e2468654079047bc05c5a4f3a59
GigaTortilla <martinappel01@gmail.com>
2024-03-28 14:28:37 +0100

    removed old AWT window

--- src/main/java/org/gigatortilla/isbn/verifier/Main.java
+++ src/main/java/org/gigatortilla/isbn/verifier/Main.java
@@ -2,9 +2,6 @@ package org.gigatortilla.isbn.verifier;
 
 public class Main {
     public static void main(String[] args) {
-        ISBNwindow window = new ISBNwindow();
-        window.createStartupScreen();
-
         MainWindow frame = new MainWindow();
         frame.setVisible(true);
     }
